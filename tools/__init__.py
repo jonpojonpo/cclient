6c79d0607eb9dc93a9921eb4a23c7bec12585fee
Jon Poole <jonpojonpo@yahoo.co.uk>
2025-02-25 19:44:11 +0000

    bump

--- tools/__init__.py
+++ tools/__init__.py
@@ -3,11 +3,14 @@ from .collection import ToolCollection
 from .bash import BashTool
 from .edit import EditTool
 
-__all__ = [
-    'BashTool',
-    'CLIResult',
-    'EditTool',
-    'ToolCollection',
-    'ToolResult',
-    'ToolManager',
+__ALL__ = [
+    BashTool20241022,
+    BashTool20250124,
+    CLIResult,
+    EditTool20241022,
+    EditTool20250124,
+    ToolCollection,
+    ToolResult,
+    ToolVersion,
+    TOOL_GROUPS_BY_VERSION,
 ]
